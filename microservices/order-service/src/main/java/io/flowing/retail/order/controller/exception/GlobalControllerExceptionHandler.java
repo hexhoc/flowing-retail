efd422b0d8e2e8a1d4dc50825d265462caa098f7
hexhoc <hexhoc@gmail.com>
2022-10-02 15:05:27 +0300

    Add controller advice and springdoc

---
+++ microservices/order-service/src/main/java/io/flowing/retail/order/controller/exception/GlobalControllerExceptionHandler.java
@@ -0,0 +1,58 @@
+package io.flowing.retail.order.controller.exception;
+
+
+import io.flowing.retail.order.dto.ApiError;
+import lombok.extern.log4j.Log4j2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@RestControllerAdvice
+@Log4j2
+public class GlobalControllerExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ApiError> processValidationError(IllegalArgumentException e) {
+        log.info("Returning HTTP 400 Bad Request", e);
+
+        ApiError apiError = new ApiError("Returning HTTP 400 Bad Request", e.getMessage());
+        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ApiError> processMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        log.info("Returning HTTP 400 Bad Request", e);
+
+        List<String> errors = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        ApiError apiError = new ApiError("Method Argument Not Valid", e.getMessage(), errors);
+
+        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    protected ResponseEntity<ApiError> handleAllExceptions(Exception e) {
+        log.info("Returning HTTP 400 Bad Request", e);
+
+        ApiError apiError = new ApiError("Internal Exception", e.getMessage());
+        return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}

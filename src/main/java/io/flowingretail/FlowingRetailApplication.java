82b502e87179f2995fa83f1e512115ad4540f598
hexhoc <hexhoc@gmail.com>
2024-04-02 22:29:56 +0300

    Refactoring

--- src/main/java/io/flowingretail/FlowingRetailApplication.java
+++ src/main/java/io/flowingretail/FlowingRetailApplication.java
@@ -2,8 +2,10 @@ package io.flowingretail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class FlowingRetailApplication {
 
 	public static void main(String[] args) {
